b12c9ee186935e14e2ae4f3e4e6ba071db115046
henristr <strickerhenri@gmail.com>
2026-02-22 13:49:56 +0100

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["smartfridge.henristr.de"], // <-- hier den Host erlauben
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
